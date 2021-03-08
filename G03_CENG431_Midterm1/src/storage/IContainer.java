05fdb8f6bf5a3953e61d42ad0a77e09a5737cb9f
Arif Burak Demiray <57103426+arifBurakDemiray@users.noreply.github.com>
2021-03-08 20:15:06 +0300

    Added storage and exception packages, also added IContainer Interface

--- G03_CENG431_Midterm1/src/storage/IContainer.java
+++ G03_CENG431_Midterm1/src/storage/IContainer.java
@@ -3,10 +3,48 @@
  */
 package storage;
 
+import exception.ItemNotFoundException;
+
 /**
  * @author burak
  *
  */
-public interface IContainer {
-
+public interface IContainer<T> {
+	/**
+	 * This function adds an elements to the container
+	 * @param item which is going to be added to the container
+	 */
+	public void add(T item);
+	/**
+	 * 	
+	 * This function returns an element by its id
+	 * @param id searched item's id
+	 * @return s searched item if not found throws 
+	 * @throws ItemNotFoundException if not found by id it throws that exception
+	 */
+	public T getById(int id) throws ItemNotFoundException;
+	
+	/**
+	 * This function removes given element
+	 * @param item that is going to be deleted
+	 * @return s the deleted item
+	 * @throws ItemNotFoundException if not found that item throw and ItemNotFound exception
+	 */
+	public T remove(T item) throws ItemNotFoundException;
+	
+	/**
+	 * This function removes element by its id
+	 * @param id the id of element that is going to be deleted
+	 * @return s deleted item
+	 * @throws ItemNotFoundException if not found
+	 */
+	public T removeById(int id) throws ItemNotFoundException;
+	
+	/**
+	 * This function returns the length of the collection
+	 * @return s the length of the collection
+	 */
+	public int getLength();
+	
+	
 }
