355cfda650e9afc300fb0b79e7490b92a35e04f7
Arif Burak Demiray <57103426+arifBurakDemiray@users.noreply.github.com>
2021-03-08 20:53:44 +0300

    Channel class and Team class

--- G03_CENG431_Midterm1/src/team/Team.java
+++ G03_CENG431_Midterm1/src/team/Team.java
@@ -1,5 +1,15 @@
 package team;
 
+import java.util.List;
+
+import channel.Channel;
+import storage.UserContainer;
+import user.TeamOwner;
+
 public class Team {
 
+	private String name;
+	private List<Channel> meeting_ch_list;
+	private UserContainer memberUsers;
+	private List<TeamOwner> owners;
 }
