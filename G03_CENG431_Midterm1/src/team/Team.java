e8e01c724245f299542713482ef5e03c8b437235
Arif Burak Demiray <57103426+arifBurakDemiray@users.noreply.github.com>
2021-03-09 14:18:29 +0300

    Update TeamOwner as Academician in Team.java

--- G03_CENG431_Midterm1/src/team/Team.java
+++ G03_CENG431_Midterm1/src/team/Team.java
@@ -6,7 +6,7 @@ import java.util.List;
 import channel.Channel;
 import storage.IContainer;
 import storage.UserContainer;
-import user.TeamOwner;
+import user.Academician;
 import user.User;
 
 public class Team {
@@ -14,11 +14,11 @@ public class Team {
 	private String name;
 	private List<Channel> meeting_ch_list;
 	private IContainer<User> memberUsers;
-	private List<TeamOwner> owners;
+	private List<Academician> owners;
 	
-	public Team(String name, TeamOwner owner) {
+	public Team(String name, Academician owner) {
 		this.name = name;
-		this.owners = new ArrayList<TeamOwner>();
+		this.owners = new ArrayList<Academician>();
 		this.owners.add(owner);
 		this.meeting_ch_list = new ArrayList<Channel>();
 		this.memberUsers = new UserContainer();
