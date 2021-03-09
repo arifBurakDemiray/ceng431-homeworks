88a35fc2fd11f10319d75cac79b328dc81447eeb
Arif Burak Demiray <57103426+arifBurakDemiray@users.noreply.github.com>
2021-03-09 16:42:14 +0300

    Update TimePeriod.java
    Look here furkan

--- G03_CENG431_Midterm1/src/enums/TimePeriod.java
+++ G03_CENG431_Midterm1/src/enums/TimePeriod.java
@@ -3,3 +3,6 @@ package enums;
 public enum TimePeriod {
 	AM,PM;
 }
+//furkan, look here You want it to use so I created, 
+//I created that way but I do not kmow that is true
+//and I opened enums package, we can store enums here
