6aceb25c2b6df2656e88307c0cbee5030eac3d30
Arif Burak Demiray <57103426+arifBurakDemiray@users.noreply.github.com>
2021-04-02 17:43:50 +0300

    little update
    Co-Authored-By: Furkan ŞAHİN <31508678+furkansahinfs@users.noreply.github.com>

--- G03_CENG431_Midterm2/src/Main.java
+++ G03_CENG431_Midterm2/src/Main.java
@@ -1,30 +1,68 @@
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
 
-import state.ProductState;
-import org.json.JSONArray;
+import org.json.*;
 
 public class Main {
-	public static void main(String[] args)
-	{
-		ProductState st = new ProductState();
-		System.out.println(st.getState());
-		
-		File file = new File("deneme.json"); // opening file
-		BufferedWriter br;
+	public static void main(String[] args) throws JSONException {
+		org.json.JSONArray js;
+		File file = new File("data\\deneme.json"); // opening file
+		BufferedReader br;
 		try {
-			br = new BufferedWriter(new FileWriter(file));
-					br.append("\nTEAMSTECH\n");// append it - TEAMSTECH indicates file is read at least one time.
-		br.close();// and close
+			br = new BufferedReader(new FileReader(file));
+			String fileAll = "";
+			String line;
+			while ((line = br.readLine()) != null) {
+				fileAll += line;
+			}
+			br.close();
+
+			JSONObject object = new JSONObject(fileAll);
+			String[] keys = JSONObject.getNames(object);
+			JSONTokener jst;
+		
+			for (String key : keys) {
+				JSONObject value = (JSONObject) object.get(key);
+				jst = new JSONTokener(value.toString());
+				JSONObject obj = (JSONObject)jst.nextValue();
+				for (String string : JSONObject.getNames(obj)) {
+					System.out.println(string);
+				}
+				
+				// Determine type of value and do something with it...
+			}
+			/*
+			//
+			JSONObject jsObject = new JSONObject(fileAll);
+			// JSONObject next = it.next();
+			loopThroughJson("", jsObject);*/
+
+			// JSONObject js1 = js.getJSONObject("Engine");
+			// System.out.println(((JSONObject) js1.get("EngineBlock")).get("type"));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} // setting up buffered writer in write mode to		
-		JSONArray js = new JSONArray();
+		}
+	}
+
+	public static void loopThroughJson(String keyOb, Object input) throws JSONException {
+
+		if (input instanceof JSONObject) {
+
+			Iterator<?> keys = ((JSONObject) input).keys();
+
+			while (keys.hasNext()) {
+
+				String key = (String) keys.next();
+				loopThroughJson(key, ((JSONObject) input).get(key));
+
+			}
+
+		} else {
+			System.out.println(keyOb + "=" + input.toString());
+		}
+
 	}
 }
-
-
-		
